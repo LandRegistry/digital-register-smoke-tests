1638efd59eb55ca5d9b6f5413326c83c053f0c9d
Pheonnexx <pheonn@hotmail.co.uk>
2015-07-01 16:33:53 +0100

    Complete smoke tests

--- drv-smoke-tests/smoke_test.rb
+++ drv-smoke-tests/smoke_test.rb
@@ -1,16 +1,79 @@
+require_relative 'test_variables'
 require 'capybara/poltergeist'
 session = Capybara::Session.new(:poltergeist)
 
 session.visit "#{$DIGITAL_REGISTER_URL}"
+
+# Check that the login page is up
 begin
   session.has_content?("Login")
 rescue
   puts "Login page unavailable"
 end
+# Confirm that you can login with correct credentials
 begin
-  fill_in 'username', with: $SMOKE_USERNAME
-  fill_in 'password', with: $SMOKE_PASSWORD
-  click_button('signin')
+  session.fill_in 'username', with: $SMOKE_USERNAME
+  session.fill_in 'password', with: $SMOKE_PASSWORD
+  session.click_button('signin')
+  session.assert_text('Search for the title of any property in England and Wales')
 rescue
   puts "Unable to login"
+end
+# Confirm that you can search with a title number
+begin
+  session.fill_in 'search_term', with: $SMOKE_TITLE_NUMBER
+  session.click_button('Search')
+  session.assert_text("#{$SMOKE_TITLE_NUMBER}")
+rescue
+  puts "Title number search has failed"
+end
+# Check that a title page is displayed
+begin
+  session.assert_text("Summary of title #{$SMOKE_TITLE_NUMBER}")
+rescue
+  puts "Title summary page has not displayed"
+end
+# Go back to search page
+begin
+  session.click_link('Find a title')
+  session.assert_text('Search for the title of any property in England and Wales')
+rescue
+  puts "Unable to navigate to search page"
+end
+# Search with postcode
+begin
+  session.fill_in 'search_term', with: $SMOKE_POSTCODE
+  session.click_button('Search')
+  session.assert_text("#{$SMOKE_TITLE_NUMBER}")
+rescue
+  puts "Postcode search has failed"
+end
+# View a title that has been searched with via postcode
+begin
+  session.click_link($SMOKE_PARTIAL_ADDRESS)
+  session.assert_text("Summary of title #{$SMOKE_TITLE_NUMBER}")
+rescue
+  puts "Unable to navigate to title summary from search page"
+end
+# Go back to search page
+begin
+  session.click_link('Find a title')
+  session.assert_text('Search for the title of any property in England and Wales')
+rescue
+  puts "Unable to navigate to search page"
+end
+# Search with address string
+begin
+  session.fill_in 'search_term', with: $SMOKE_PARTIAL_ADDRESS
+  session.click_button('Search')
+  session.assert_text("#{$SMOKE_TITLE_NUMBER}")
+rescue
+  puts "Address string search has failed"
+end
+# View a title that has been searched with via postcode
+begin
+  session.click_link($SMOKE_PARTIAL_ADDRESS)
+  session.assert_text("Summary of title #{$SMOKE_TITLE_NUMBER}")
+rescue
+  puts "Unable to navigate to title summary from search page"
 end
---
+++ drv-smoke-tests/test_variables.rb
@@ -0,0 +1,7 @@
+$DIGITAL_REGISTER_URL = ENV.fetch('DIGITAL_REGISTER_URL', 'http://digital-register-frontend.landregistry.local:8003/login')
+$SMOKE_USERNAME = ENV.fetch('SMOKE_USERNAME', 'userid123')
+$SMOKE_PASSWORD = ENV.fetch('SMOKE_PASSWORD', 'password123')
+#Remember: You need to replace title data if running this on your machine.
+$SMOKE_TITLE_NUMBER = ENV.fetch('SMOKE_TITLE_NUMBER', 'DN547598')
+$SMOKE_PARTIAL_ADDRESS = ENV.fetch('SMOKE_ADDRESS_STRING', '3 Test Street, Plymouth, PL9 8TB')
+$SMOKE_POSTCODE = ENV.fetch('SMOKE_POSTCODE', 'PL9 8TB')
